d40ded132e238ff3ed8d56672692afca363b9679
Skion6 <ozwemer@gmail.com>
2026-04-05 18:29:52 -0700

    Update installapps.sh
    hope for small image plsss

--- root/installapps.sh
+++ root/installapps.sh
@@ -15,28 +15,6 @@ if jq ".defaultapps | contains([2])" "$json_file" | grep -q true; then
     chmod +x /installable-apps/xarchiver.sh
     /installable-apps/xarchiver.sh
 fi
-if jq ".defaultapps | contains([3])" "$json_file" | grep -q true; then
-    chmod +x /installable-apps/flathub.sh
-    /installable-apps/flathub.sh
-fi
-
-if jq ".programming | contains([0])" "$json_file" | grep -q true; then
-    chmod +x /installable-apps/openjdk-8-jre.sh
-    /installable-apps/openjdk-8-jre.sh
-fi
-if jq ".programming | contains([1])" "$json_file" | grep -q true; then
-    chmod +x /installable-apps/openjdk-17-jre.sh
-    /installable-apps/openjdk-17-jre.sh
-fi
-
-if jq ".apps | contains([0])" "$json_file" | grep -q true; then
-    chmod +x /installable-apps/vlc.sh
-    /installable-apps/vlc.sh
-fi
-if jq ".apps | contains([1])" "$json_file" | grep -q true; then
-    chmod +x /installable-apps/libreoffice.sh
-    /installable-apps/libreoffice.sh
-fi
 if jq ".apps | contains([2])" "$json_file" | grep -q true; then
     chmod +x /installable-apps/synaptic.sh
     /installable-apps/synaptic.sh
